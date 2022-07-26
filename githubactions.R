1748f319e34b3000f38906633b3edaf83e2c685d
nate-d-olson <nolson@nist.gov>
2022-07-26 20:36:45 +0000

    updating script

--- githubactions.R
+++ githubactions.R
@@ -2,6 +2,6 @@
 usethis::use_github_action(url = "https://raw.githubusercontent.com/rstudio/actions/main/examples/connect-publish.yaml")
 
 usethis::use_github_action(
-  url = "https://github.com/rstudio/shinytest2/raw/main/actions/test-app/example-test-app-renv.yaml",
+  url = "https://raw.githubusercontent.com/rstudio/shinytest2/main/actions/test-app/example-test-app-renv.yaml",
   save_as = "test-app-renv.yaml"
 )
